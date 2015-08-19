a1fea1ccfa7bef1dac4b0a59a1507229953b360c
zyxalequa <wahooimagonna@gmail.com>
2015-08-19 15:06:17 +0300

    Added the element enum. Along with a test item for it.

---
+++ src/main/java/com/thexfactor117/ce/items/ItemElement.java
@@ -0,0 +1,33 @@
+package com.thexfactor117.ce.items;
+
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
+
+import com.thexfactor117.ce.enums.ElementEnum;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+/**
+ * @author Hlaaftana
+ * THIS IS JUST A TEST FOR THE ELEMENT ENUMS
+ * YOU CAN DELETE THIS LATER X
+ */
+public class ItemElement extends ItemCE{
+	private ElementEnum element;
+	public ItemElement(ElementEnum element) {
+		super(element.name.toLowerCase());
+		this.element = element;
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean isHeld){
+		list.add(EnumChatFormatting.WHITE + element.name + ", " + element.formula + ", " + element.atomicNumber + "\n");
+		list.add(EnumChatFormatting.WHITE + element.stateAtRoomTemperature + "\n");
+	}
+}
