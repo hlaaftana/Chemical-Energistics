2700668c6a5fc763e8c21f741c83a6f43e2a9e4f
hlaaftana <atadorukaydin@gmail.com>
2015-08-20 10:59:57 +0300

    What the heck...

---
+++ src/main/java/com/thexfactor117/ce/handlers/BucketHandler.java
@@ -0,0 +1,61 @@
+package com.thexfactor117.ce.handlers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cpw.mods.fml.common.eventhandler.Event.Result;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.FillBucketEvent;
+
+
+public class BucketHandler extends FuelHandler{
+
+	public static BucketHandler INSTANCE = new BucketHandler();
+	public Map<Block, Item> buckets = new HashMap<Block, Item>();
+	protected BucketHandler() {
+	}
+
+	@SubscribeEvent
+	public void onBucketFill(FillBucketEvent event) {
+
+			ItemStack result = fillCustomBucket(event.world, event.target);
+
+			if (result == null)
+				return;
+
+			event.result = result;
+			event.setResult(Result.ALLOW);
+	}
+
+	private ItemStack fillCustomBucket(World world, MovingObjectPosition pos) {
+
+		Block block = world.getBlock(pos.blockX, pos.blockY, pos.blockZ);
+
+		Item bucket = buckets.get(block);
+		if (bucket != null && world.getBlockMetadata(pos.blockX, pos.blockY, pos.blockZ) == 0) {
+			world.setBlockToAir(pos.blockX, pos.blockY, pos.blockZ);
+			return new ItemStack(bucket);
+		} else
+			return null;
+	}
+		
+	/**
+	 * Register a bucket for a fluid's block.
+	 * @param block The fluid's block.
+	 * @param bucket The filled bucket.
+	 */
+	@SuppressWarnings("unused")
+	private static void registerBucket(Block block, Item bucket){
+		BucketHandler.INSTANCE.buckets.put(block, bucket);
+	}
+	public static void registerBuckets(){
+		//registerBucket(exampleFluid, exampleBucket);
+		MinecraftForge.EVENT_BUS.register(BucketHandler.INSTANCE);
+	}  
+}
---
+++ src/main/java/com/thexfactor117/ce/world/CEWorldGeneration.java
@@ -0,0 +1,117 @@
+package com.thexfactor117.ce.world;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+
+import com.thexfactor117.ce.handlers.ConfigHandler;
+import com.thexfactor117.ce.init.CEBlocks;
+
+import cpw.mods.fml.common.IWorldGenerator;
+
+public class CEWorldGeneration implements IWorldGenerator
+{	
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider) 
+	{
+		switch (world.provider.dimensionId)
+		{
+    		case 0:
+    			generateSurface(world, random, chunkX * 16, chunkZ * 16);
+    		case -1:
+    			generateNether(world, random, chunkX * 16, chunkZ * 16);
+    		case 1:
+    			generateEnd(world, random, chunkX * 16, chunkZ * 16);
+		}
+	}
+	
+	private void generateSurface(World world, Random random, int x, int z)
+	{	
+		if (ConfigHandler.generateAluminum)
+		{
+			addOreSpawn(CEBlocks.oreAluminum, world, random, x, z, 2 + random.nextInt(6), 25, 0, 64);
+		}
+		
+		if (ConfigHandler.generateSulfur)
+		{
+			addOreSpawn(CEBlocks.oreSulfur, world, random, x, z, 2 + random.nextInt(4), 20, 0, 48);
+		}
+		
+		if (ConfigHandler.generateUranium)
+		{
+			addOreSpawn(CEBlocks.oreUranium, world, random, x, z, 2 + random.nextInt(2), 15, 0, 16);
+		}
+		
+		if (ConfigHandler.generateIridium)
+		{
+			addOreSpawn(CEBlocks.oreIridium, world, random, x, z, 1, 10, 0, 64);
+		}
+	}
+	
+	private void generateNether(World world, Random random, int x, int z) {}
+
+	private void generateEnd(World world, Random random, int x, int z) {}
+	
+	/**
+	 * Generates specified ore in the overworld.
+	 * @param block The ore to spawn.
+	 * @param maxVeinSize The amount of ores in one vein.
+	 * @param chanceToSpawn The ore's chance to spawn.
+	 * @param minY The minimum Y coordinate.
+	 * @param maxY The maximum Y coordinate.
+	 */
+	private void addOreSpawn(Block block, World world, Random random, int blockXPos, int blockZPos, int maxVeinSize, int chanceToSpawn, int minY, int maxY)
+	{
+		for (int i = 0; i < chanceToSpawn; i++)
+		{
+			int posX = blockXPos + random.nextInt(16);
+			int posY = minY + random.nextInt(maxY - minY);
+			int posZ = blockZPos + random.nextInt(16);
+			new WorldGenMinable(block, maxVeinSize).generate(world, random, posX, posY, posZ);
+		}
+	}
+	
+	/**
+	 * Generates specified ore in the nether.
+	 * @param block The ore.
+	 * @param maxVeinSize The amount of ores in one vein.
+	 * @param chanceToSpawn The ore's chance to spawn.
+	 * @param minY The minimum Y coordinate.
+	 * @param maxY The maximum Y coordinate.
+	 */
+	@SuppressWarnings("unused")
+	private void addNetherOreSpawn(Block block, World world, Random random, int blockXPos, int blockZPos, int maxVeinSize, int chanceToSpawn, int minY, int maxY)
+	{
+		for (int i = 0; i < chanceToSpawn; i++)
+		{
+			int posX = blockXPos + random.nextInt(16);
+			int posY = minY + random.nextInt(maxY - minY);
+			int posZ = blockZPos + random.nextInt(16);
+			new WorldGenMinable(block, maxVeinSize, Blocks.netherrack).generate(world, random, posX, posY, posZ);
+		}
+	}
+	
+	/**
+	 * Generates specified ore in the nether.
+	 * @param block The ore.
+	 * @param maxVeinSize The amount of ores in one vein.
+	 * @param chanceToSpawn The ore's chance to spawn.
+	 * @param minY The minimum Y coordinate.
+	 * @param maxY The maximum Y coordinate.
+	 */
+	@SuppressWarnings("unused")
+	private void addEndOreSpawn(Block block, World world, Random random, int blockXPos, int blockZPos, int maxVeinSize, int chanceToSpawn, int minY, int maxY)
+	{
+		for (int i = 0; i < chanceToSpawn; i++)
+		{
+			int posX = blockXPos + random.nextInt(16);
+			int posY = minY + random.nextInt(maxY - minY);
+			int posZ = blockZPos + random.nextInt(16);
+			new WorldGenMinable(block, maxVeinSize, Blocks.end_stone).generate(world, random, posX, posY, posZ);
+		}
+	}
+}
